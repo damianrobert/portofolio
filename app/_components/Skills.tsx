7de8199ca33b419ffa141f2897c89e9bbb437399
damianrobert <damianrobert666@yahoo.com>
2026-06-04 15:10:03 +0300

    update sad server badge

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -106,7 +106,7 @@ export default function Skills() {
             rel="noopener noreferrer"
           >
             <img
-              src="https://img.shields.io/badge/SadServers-Advanced-7B1FA2?style=for-the-badge&labelColor=FFC400&logo=terraform&logoColor=1A237E&logoSize=auto"
+              src="https://img.shields.io/badge/SadServers-Master-FF6D00?style=for-the-badge&labelColor=FFC400&logo=kubernetes&logoColor=1A237E&logoSize=auto"
               alt="SadServers Level"
             />
           </a>
