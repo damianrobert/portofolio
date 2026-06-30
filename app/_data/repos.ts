b0c28c96d4ab86a4b6244ae365ab2728a325089c
damianrobert <damianrobert666@yahoo.com>
2026-06-30 15:25:03 +0300

    added miscroservices app repo

--- app/_data/repos.ts
+++ app/_data/repos.ts
@@ -3,4 +3,5 @@ export const GITHUB_REPOS: string[] = [
   "https://github.com/damianrobert/automated-backup-system",
   "https://github.com/damianrobert/kubernetes-the-hard-way",
   "https://github.com/damianrobert/web-app-deploy",
+  "https://github.com/damianrobert/microservices-app",
 ];
